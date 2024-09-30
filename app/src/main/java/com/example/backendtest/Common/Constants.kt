030a8cbf6369111c5069939d984c809852ae08b2
Cho Hyung Seok <112924213+cho123456789@users.noreply.github.com>
2024-09-30 23:58:45 +0900

    안드로이드 통신 모듈 업데이트

---
+++ app/src/main/java/com/example/backendtest/Common/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.backendtest.Common
+
+object Constants {
+
+}
---
+++ app/src/main/java/com/example/backendtest/repository/ui/MainScreen.kt
@@ -0,0 +1,8 @@
+package com.example.backendtest.repository.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(){
+    TextScreen()
+}
